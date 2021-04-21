91dd33fec88673838f1e6aba6842176bacf432dc
StanislavEng <ictstas@gmail.com>
2021-04-20 22:12:12 -0700

    Fixed RPi and Arduino I2C programs for controlling 3 LED independently to toggle on and off

--- tryI2C.py
+++ tryI2C.py
@@ -5,7 +5,7 @@ addr = 0x8 # bus address
 bus = SMBus(1) #indicates I2C bus 1 in /dev/ic2-1
 
 numb = 1
-
+datSend = 0
 
 while numb == 1:
     print("Do you want to turn on red, blue or yellow LED?")
@@ -19,17 +19,26 @@ while numb == 1:
     #    numb = 0
 
     if whatLED == "red":
-        bus.write_byte(addr,0x1)
+#        bus.write_byte(addr,0x1)
+        datSend = 0x1
     elif whatLED == "blue":
-        bus.write_byte(addr,0x2)
+#        bus.write_byte(addr,0x2)
+        datSend = 0x2
     elif whatLED == "yellow":
-        bus.write_byte(addr,0x4)
+#        bus.write_byte(addr,0x4)
+        datSend = 0x4
     else:
         numb = 0
-    sleep(0.5)
-    ledstate = input("\nPress 1 to turn ON or 0 to turn OFF ")
+#    sleep(0.5)
+    print("Press 1 to turn ON or 0 to turn OFF ")
+    ledstate = input(">>> ")
     if ledstate == "1":
-        bus.write_byte(addr,0x1)
+#        bus.write_byte(addr,0x1)
+        datSend = (datSend<< 1 ) | 0x1
     else:
-        bus.write_byte(addr,0x0)
+#        bus.write_byte(addr,0x0)
+        datSend = (datSend<< 1 )
+    bus.write_byte(addr,datSend)
     print("You said: ", whatLED, " and ", ledstate)
+    print("Your data is: " , datSend)
+    
